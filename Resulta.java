53df62b88ae3dc62cf8409848deac7320850bc06
AbdellahTezza <76401409+AbdellahTezza@users.noreply.github.com>
2022-08-22 23:55:32 +0100

    Update Resulta.java

--- Resulta.java
+++ Resulta.java
@@ -17,6 +17,7 @@ public class Resultanote1 {
 	private static double totleArabe;
 	private static double totleInfor;
 	private static double totoalall;
+	private static double notemath;
 	public static void main(String[] args) {
 		
 		//double  total = 0; 
@@ -36,23 +37,23 @@ public class Resultanote1 {
 	    ArrayList<Double> matieremath = new ArrayList<>();
 	    System.out.print("Enter votre math : ");
 	    math = scan.nextDouble();
-	    totlemath  =  (math  *  4)/ 10;
-	    System.out.println ( "Note de math est : "  +  totlemath   ); 
+	    totlemath  =  (math  *  40)/ 100;
+	    //System.out.println ( "Note de math est : "  +  totlemath   ); 
 	    
 	    /* matier Info */
 	    ArrayList<Double> matiereinfo = new ArrayList<>();
 	    System.out.print("Enter votre note Info : ");
 	    Info = scan.nextDouble();
-	    totleInfor  =  (Info  *  3 )/ 10;
-	    System.out.println ( "Note de Info est : "  +  totleInfor  ); 
+	    totleInfor  =  (Info  *  30 )/ 100;
+	    //System.out.println ( "Note de Info est : "  +  totleInfor  ); 
 	   // 
 	    
 	    /* matier Arabe */
 	    ArrayList<Double> matierearabe = new ArrayList<>();
 	    System.out.print("Enter votre note Arabe : ");
 	    Arabe = scan.nextDouble();
-	    totleArabe  =  (Arabe  *  3)/ 10;
-	    System.out.println ( "Note de Arabe est : "  +  totleArabe  ); 
+	    totleArabe  =  (Arabe  *  30)/ 100;
+	   // System.out.println ( "Note de Arabe est : "  +  totleArabe  ); 
 	    //
 	    matieremath.add(totlemath);
 	    matiereinfo.add(totleInfor);
@@ -62,8 +63,7 @@ public class Resultanote1 {
 	    resulta.put("Math", matieremath);
 	    resulta.put("Info", matiereinfo);
 	    resulta.put("Arabe", matierearabe);
-	    totoalall = ((totleArabe * 3) + (totleInfor * 3) + (totlemath * 4))/10;
-	    System.out.println( resulta ); 
+	    totoalall = (totleArabe * 30  + totleInfor * 30 + totlemath * 40)/100;
 	    System.out.println( "Résultat finale de  " + Nom.get(0) +"  est :" + totoalall ); 
 	    
 	    
